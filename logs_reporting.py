42f9f029ab8a036716d8509ae5d5cdd3c5515b9c
Weder Ribas <wederribas@gmail.com>
2017-08-21 00:05:03 -0300

    Refact script to be modular and fix minor issues

--- logs_reporting.py
+++ logs_reporting.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # -*- coding: utf-8 -*-
 
@@ -16,13 +16,35 @@ retrieve useful information from logs in a web server.
 import psycopg2
 
 
-def main():
-    """Run three SQL queries against the database and print out the results"""
+def db_connect():
+    """Creates a database connection and returns the connection along with
+        the database instance.
+
+        Returns:
+            db, c - a tuple. The first element is a connection do the database
+                    The second element is a curse for the database.
+    """
     db = psycopg2.connect("dbname=news")
     conn = db.cursor()
+    return db, conn
 
-    print("1. What are the most popular three articles of all time?")
-    conn.execute("""
+
+def exec_query(query):
+    """Executes a given query in the database and returns all found rows as a
+        list of tuples.
+
+        Returns: a list of tuples containg the database output
+    """
+    db, conn = db_connect()
+    conn.execute(query)
+    results = conn.fetchall()
+    db.close()
+    return results
+
+
+def print_top_articles():
+    """Retrieve the three most accessed articles from the web server logs"""
+    query = """
         WITH articles_logs AS (
             SELECT replace(path, '/article/', '') AS slug,
                    count(*) as total_accesses
@@ -34,15 +56,20 @@ def main():
         SELECT articles.title, articles_logs.total_accesses
         FROM articles
         INNER JOIN articles_logs ON articles_logs.slug = articles.slug
-        ORDER BY total_accesses DESC;
-    """)
-    article_output = "\n%s -- %s views"
-    article_output = "".join(article_output % (title, accesses)
-                             for title, accesses in conn.fetchall())
-    print(article_output)
+        ORDER BY total_accesses DESC
+        LIMIT 3;
+    """
+    print("1. What are the most popular three articles of all time?\n")
+    results = exec_query(query)
+    for title, accesses in results:
+        print('{} -- {} views'.format(title, accesses))
 
-    print("\n2. Who are the most popular article authors of all time?")
-    conn.execute("""
+
+def print_top_authors():
+    """Print the rank of most popular authors based on the accesses to their
+       articles.
+    """
+    query = """
         WITH articles_logs AS (
             SELECT replace(path, '/article/', '') AS slug,
                    count(*) as total_accesses
@@ -61,14 +88,18 @@ def main():
         FROM authors
         INNER JOIN author_views ON author_views.author = authors.id
         ORDER BY author_views.total DESC;
-    """)
-    authors_output = "\n%s -- %s views"
-    authors_output = "".join(authors_output % (author, views)
-                             for author, views in conn.fetchall())
-    print(authors_output)
+    """
+    print("\n2. Who are the most popular article authors of all time?\n")
+    results = exec_query(query)
+    for name, total in results:
+        print('{} -- {} views'.format(name, total))
 
-    print("\n3. On which days did more than 1% of requests lead to errors?")
-    conn.execute("""
+
+def print_errors_over_one():
+    """Prints out the days where more than 1% of logged access
+       requests were errors.
+    """
+    query = """
         WITH status_logs AS (
         SELECT to_char(time, 'FMMonth DD, YYYY') AS log_date,
                count(
@@ -78,18 +109,26 @@ def main():
                count(*) AS total_accesses
         FROM log
         GROUP BY 1
-        )
-        SELECT log_date,
+        ),
+        general_logs AS (
+            SELECT log_date,
                round((total_errors * 100)::numeric
                     / total_accesses, 2) AS percent_error
-        FROM status_logs
-        GROUP BY log_date, total_errors, total_accesses
+            FROM status_logs
+            GROUP BY log_date, total_errors, total_accesses
+        )
+        SELECT log_date, percent_error
+        FROM general_logs
+        WHERE percent_error > 1
         ORDER BY 2 DESC;
-    """)
-    errors_output = '\n%s -- %s%% errors'
-    errors_output = "".join(errors_output % (date, percentages)
-                            for date, percentages in conn.fetchall())
-    print(errors_output)
+    """
+    print("\n3. On which days did more than 1% of requests lead to errors?\n")
+    results = exec_query(query)
+    for date, percent_error in results:
+        print('{} -- {}% errors'.format(date, percent_error))
+
 
 if __name__ == '__main__':
-    main()
+    print_top_articles()
+    print_top_authors()
+    print_errors_over_one()
